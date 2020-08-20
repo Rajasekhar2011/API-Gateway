dc5d7285dba7ae82e40b8bcfda7392e92d63b424
RajasekharReddy.Kolli <rajasekharreddy.kolli@cognizant.com>
2020-08-20 19:02:05 +0530

    apigateway

---
+++ src/test/java/com/springreactive/neflixzuulapigateway/NeflixZuulApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springreactive.neflixzuulapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeflixZuulApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
